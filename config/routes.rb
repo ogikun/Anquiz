d1cdb6fc351ecb8d61bb094e3d0d6b8d56a7da84
ogikun <g1621362@gmail.com>
2021-03-12 01:20:03 +0900

    [update] authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users, path_names: { edit: 'password_edit' }, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  scope module: :users do
+    get '/mypage', to: 'users#mypage', as: 'mypage'
+    get '/edit', to: 'users#edit', as: 'edit_users'
+    patch '/updateinfo', to: 'users#updateInfo', as: 'updateinfo'
+  end
 end
