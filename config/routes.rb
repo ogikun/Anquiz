55c9136641e3c84c7f9a7a1ef4d0029e7934bb5f
ogikun <g1621362@gmail.com>
2021-03-19 19:25:54 +0900

    [update] ransack

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, path_names: { edit: 'password_edit' }, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  get '/search', to: 'searches#search', as: 'search'
   scope module: :users do
     get '/mypage', to: 'users#mypage', as: 'mypage'
     get '/bookmark', to: 'users#bookmark', as: 'bookmark'
