e63278a453fa9dc7aa40c3e1ace48436461bea2e
ogikun <g1621362@gmail.com>
2021-04-07 04:46:02 +0900

    [update] delete add_words

--- db/migrate/20210311170841_create_words.rb
+++ db/migrate/20210311170841_create_words.rb
@@ -2,6 +2,7 @@ class CreateWords < ActiveRecord::Migration[5.2]
   def change
     create_table :words do |t|
       t.references :user, null: false, foreign_key: true
+      t.references :list, null: false, foreign_key: true
       t.string :question, null: false
       t.string :answer, null: false
       t.text :description, null: false
