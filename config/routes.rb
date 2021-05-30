ad0e208050478a70a1a58a618c9e438a708f0672
ogikun <g1621362@gmail.com>
2021-05-30 22:02:27 +0900

    omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admins
-  devise_for :users, path_names: { edit: 'password_edit' }, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+  devise_for :users, path_names: { edit: 'password_edit' }, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get '/search', to: 'searches#search', as: 'search'
